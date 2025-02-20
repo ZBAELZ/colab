80cde645223f8b9288b0d112b77d36be416cc828
LUIS <161374871+ZBAELZ@users.noreply.github.com>
2025-02-19 21:37:03 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const keepServerAlive = () => {
   setInterval(() => {
-    axios.get('https://colab.research.google.com/drive/1jxnXJIPDM5DRw6Le4Wd2TCsKfgNAhTjp?usp=sharing')  // Reemplaza con la URL de tu servidor de Aternos
+    axios.get('https://colab.research.google.com/drive/1sJlLSsgtM8nXIqFWoRbB0Ryj_JFcojfy?usp=sharing')  // Reemplaza con la URL de tu servidor de Aternos
       .then(response => {
         console.log("Solicitud exitosa, el servidor sigue activo.");
       })
